899528dfe2d88d59387b58e97239a316c24683ff
bangbang444 <140622878+bangbang444@users.noreply.github.com>
2025-06-12 16:14:35 +0900

    [Bronze V] Title: A+B, Time: 212 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int a  = sc.nextInt();
+        int b  = sc.nextInt();
+
+        int sum;
+
+        sum = a+b;
+
+        System.out.println(sum);
+
+    }
+}

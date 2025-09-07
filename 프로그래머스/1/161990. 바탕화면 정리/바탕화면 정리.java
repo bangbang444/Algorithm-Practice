e37d13fe32ef95c32b428101931b112c0d7e7539
bangbang444 <140622878+bangbang444@users.noreply.github.com>
2025-09-07 16:07:36 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.17 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -4,24 +4,24 @@ class Solution {
         int row = wallpaper.length;
         int col = wallpaper[0].length();
         
-        int[] xMin = {51, 51};
-        int[] yMin = {51, 51};
-        int[] xMax = {-1, -1};
-        int[] yMax = {-1, -1};
+        int xMin = 51;
+        int yMin = 51;
+        int xMax = -1;
+        int yMax = -1;
         
         // x최소, x최대 나타는 곳 구하기
         // y최소, y최대 나타는 곳 구하기
         for(int i = 0; i < row; i++){
             for(int j = 0; j < col; j++){
                 if(wallpaper[i].charAt(j) == '#'){
-                    if(i < xMin[0]) {xMin[0] = i; xMin[1] = j;}
-                    if(j < yMin[1]) {yMin[0] = i; yMin[1] = j;}
-                    if(i > xMax[0]) {xMax[0] = i; xMax[1] = j;}
-                    if(j > yMax[1]) {yMax[0] = i; yMax[1] = j;}
+                    if(i < xMin) {xMin = i;}
+                    if(j < yMin) {yMin = j;}
+                    if(i > xMax) {xMax = i;}
+                    if(j > yMax) {yMax = j;}
                 }
             }
         }
         // x,y 최소 / x,y좌표 최대        
-        return new int[]{xMin[0], yMin[1], xMax[0]+1, yMax[1]+1};
+        return new int[]{xMin, yMin, xMax+1, yMax+1};
     }
 }

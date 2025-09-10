57f00832809c5ea63262e133c2edb1cb6b33ff00
bangbang444 <140622878+bangbang444@users.noreply.github.com>
2025-09-10 23:49:18 +0900

    [level 1] Title: 소수 찾기, Time: 181.75 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int n) {
+        
+        int count = 0;
+        for(int i = 2; i <= n; i++){
+            if(isPrime(i)) count++;
+        }
+        
+        return count;
+    }
+    
+    private boolean isPrime(int num){
+        for(int i = 2; i <= (int)Math.sqrt(num); i++){
+            if(num%i==0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

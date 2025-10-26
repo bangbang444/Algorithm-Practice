209536a0ad90146f725f1416a9e70b31024f00eb
Byunghyeon <140622878+bangbang444@users.noreply.github.com>
2025-10-26 17:31:22 +0900

    [level 2] Title: 기능개발, Time: 3.18 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -26,10 +26,9 @@ class Solution {
         }
         list.add(count);
         
-        int[] answer = new int[list.size()];
-        for(int i = 0; i < list.size(); i++){
-            answer[i] = list.get(i);
-        }
+        int[] answer = list.stream()
+                            .mapToInt(Integer::intValue)
+                            .toArray();
         
         return answer;
     }

05362a7c94c8fd0b335ee50806c79e927bcaa192
Byunghyeon <140622878+bangbang444@users.noreply.github.com>
2025-10-26 17:31:04 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586#) 
 
 ### 성능 요약
 
-메모리: 88 MB, 시간: 0.05 ms
+메모리: 71 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 26일 17:11:12
+2025년 10월 26일 17:31:02
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -5,7 +5,7 @@ class Solution {
         
         List<Integer> list = new ArrayList<>();
 
-        int date = 0;
+        int date = (100-progresses[0]-1)/speeds[0]+1;
         int count = 0;
         int progressCount = 0;
         int idx = 0;
@@ -26,9 +26,9 @@ class Solution {
         }
         list.add(count);
         
-        int[] answer = new int[list.size()-1];
-        for(int i = 0; i < list.size()-1; i++){
-            answer[i] = list.get(i+1);
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            answer[i] = list.get(i);
         }
         
         return answer;

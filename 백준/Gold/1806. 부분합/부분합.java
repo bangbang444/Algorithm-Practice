eca3399bd3889b4a878cbae32136aefda29623ec
Byunghyeon <140622878+bangbang444@users.noreply.github.com>
2025-12-12 17:23:47 +0900

    [Gold IV] Title: 부분합, Time: 284 ms, Memory: 24356 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] seqs = new int[N+1];
+        for(int i = 1; i <= N; i++){
+            seqs[i] += seqs[i-1] + Integer.parseInt(st.nextToken());
+        }
+
+        int min = Integer.MAX_VALUE;
+        int left = 0, right = N;
+        while(right <= N && left < right){
+
+            if(seqs[right]-seqs[left] >= S){
+                right--;
+                min = Math.min(right-left+1, min);
+            }else{
+                left++;
+                while(right <= N && seqs[right]-seqs[left] < S){
+                    right++;
+                }
+            }
+
+        }
+
+        System.out.println(min == Integer.MAX_VALUE ? 0 : min);
+    }
+}

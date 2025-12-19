a32825159eaa8781b7f552f929cbdb669be6cbfa
Byunghyeon <140622878+bangbang444@users.noreply.github.com>
2025-12-19 14:44:23 +0900

    [Gold IV] Title: 공유기 설치, Time: 288 ms, Memory: 29160 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,69 +1,58 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static int N;
-    static int C;
-    static ArrayList<Integer> coordinates;
-
-    static boolean[] isSelected;
-
-    static ArrayList<Integer> small;
-
+    static int N,C;
+    static int[] share;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        //  5 3
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
 
-        isSelected = new boolean[N];
-        coordinates = new ArrayList<>();
+        share = new int[N];
         for(int i = 0; i < N; i++){
-            st = new StringTokenizer(br.readLine());
-            int coordinate = Integer.parseInt(st.nextToken());
-            coordinates.add(coordinate);
+            share[i] = Integer.parseInt(br.readLine());
         }
+
         // 1 2 4 8 9
-        Collections.sort(coordinates);
+        Arrays.sort(share);
 
         // 거리
-        int first = 1; // 1
-        int last = coordinates.get(coordinates.size()-1) - coordinates.get(0); // 9
+        int first = 1;
+        int last = share[share.length-1] - share[0];
 
         int answer = 1;
         while(first <= last){
-            int mid = (first + last) / 2; // 찾는 거리
+            int mid = (first + last) / 2;
+
             if(check(mid)){
                 answer = mid;
                 first = mid + 1;
             }else{
                 last = mid - 1;
             }
+
         }
 
         System.out.println(answer);
     }
 
-    private static boolean check(int distance) {
-
-        int count = 1; // 공유기
-        int start = coordinates.get(0);
-
+    public static boolean check(int distance){
+        int count = 1;
+        int start = share[0];
         for(int i = 1; i < N; i++){
-            if(coordinates.get(i) - start >= distance){
+            if(share[i] - start >= distance){
                 count++;
-                start = coordinates.get(i);
+                start = share[i];
             }
         }
 
         return count >= C;
     }
-
 }
+

5ffe5af58e4fa9778560d85e0ec7f8f0430fe30c
Byunghyeon <140622878+bangbang444@users.noreply.github.com>
2026-01-03 01:01:52 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 114.06 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -2,30 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] elements) {
-        int eleng = elements.length;
-        int[] el2 = new int[eleng*2+1];
-        
-        for(int i = 0; i < eleng*2; i++){
-            el2[i+1] = elements[i%eleng] + el2[i];
-        }
-        
+        int eleng = elements.length;        
         Set<Integer> set = new HashSet<>();
-        int p1 = 0, p2 = 1;
-        while(p2+eleng < el2.length){
-            
-            for(int i = p2; i < p2+eleng; i++){
-                int sum = el2[i]-el2[p1];
-                if(!set.contains(sum)) set.add(sum);
+        
+        int[] sums = new int[elements.length];
+        for(int i = 1; i <= elements.length; i++){
+            for(int j = 0; j < elements.length; j++){
+                sums[j] += elements[(i+j-1)%elements.length];
+                set.add(sums[j]);
             }
-            
-            p1++;
-            p2=p1+1;
         }
         
         return set.size();
     }
 }
-
-
-//[7, 9, 1, 1, 4, 7, 9, 1, 1, 4]
-//[0, 7, 16, 17, 18, 22, 29, 38, 39, 40, 44]

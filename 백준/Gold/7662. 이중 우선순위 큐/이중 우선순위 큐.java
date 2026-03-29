73bf08782d1b003621e8bc358958c4c7cc969131
Byunghyeon <140622878+bangbang444@users.noreply.github.com>
2026-03-30 00:47:08 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2692 ms, Memory: 431212 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
 import java.util.TreeMap;
 
 public class Main {
@@ -13,52 +12,48 @@ public class Main {
         for(int i = 0; i < T; i++){
             int k = Integer.parseInt(br.readLine());
 
-            TreeMap<Integer, Integer> minMap = new TreeMap<>();
-            TreeMap<Integer, Integer> maxMap = new TreeMap<>(Collections.reverseOrder());
+            TreeMap<Integer, Integer> map = new TreeMap<>();
             for(int j = 0; j < k; j++){
                 String[] cmd = br.readLine().split(" ");
                 String op = cmd[0];
                 int n = Integer.parseInt(cmd[1]);
 
                 if(op.equals("I")){
-                    insertCmd(n, minMap, maxMap);
+                    insertCmd(n, map);
                 }else if(op.equals("D")){
-                    if(n == 1 && !maxMap.isEmpty()){
-                        deleteCmd(maxMap, minMap);
-                    }else if(n == -1 && !minMap.isEmpty()){
-                        deleteCmd(minMap, maxMap);
-                    }
+                    deleteCmd(map, n);
                 }
             }
 
             // 후처리
-            if(maxMap.isEmpty()){
+            if(map.isEmpty()){
                 answer.append("EMPTY").append("\n");
-            }else if(maxMap.size() == 1){
-                answer.append(maxMap.firstKey()).append(" ").append(maxMap.firstKey()).append("\n");
+            }else if(map.size() == 1){
+                answer.append(map.firstKey()).append(" ").append(map.firstKey()).append("\n");
             }else{
-                answer.append(maxMap.firstKey()).append(" ").append(minMap.firstKey()).append("\n");
+                answer.append(map.lastKey()).append(" ").append(map.firstKey()).append("\n");
             }
         }
 
         System.out.println(answer.toString().trim());
     }
 
-    private static void insertCmd(Integer n, TreeMap<Integer, Integer> minMap, TreeMap<Integer, Integer> maxMap) {
-        minMap.put(n, minMap.getOrDefault(n, 0)+1);
-        maxMap.put(n, maxMap.getOrDefault(n, 0)+1);
+    private static void insertCmd(Integer n, TreeMap<Integer, Integer> map) {
+        map.put(n, map.getOrDefault(n, 0)+1);
     }
 
-    private static void deleteCmd(TreeMap<Integer, Integer> stdMap, TreeMap<Integer, Integer> sideMap){
-        int value = stdMap.firstKey();
+    private static void deleteCmd(TreeMap<Integer, Integer> map, int flag){
+        if(map.isEmpty()) return;
 
-        int cnt = stdMap.getOrDefault(value, 1);
+        int min = map.firstKey();
+        int max = map.lastKey();
+        int value = flag == 1 ? max : min;
+
+        int cnt = map.getOrDefault(value, 1);
         if(cnt <= 1){
-            stdMap.remove(value);
-            sideMap.remove(value);
+            map.remove(value);
         }else{
-            stdMap.put(value, stdMap.get(value)-1);
-            sideMap.put(value, sideMap.get(value)-1);
+            map.put(value, map.get(value)-1);
         }
     }
 }

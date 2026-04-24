c14baab306b784cc823913e6924c1203842950f5
Byunghyeon <140622878+bangbang444@users.noreply.github.com>
2026-04-25 01:25:16 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,28 +1,31 @@
 -- 사원별 성과금 정보를 조회
 -- 사번, 성명, 평가 등급, 성과금을 조회
+WITH EMP_AVG AS (
+    SELECT EMP_NO, ROUND(AVG(SCORE), 2) AS AVG_SCORE
+    FROM HR_GRADE
+    GROUP BY EMP_NO
+)
 SELECT  HE.EMP_NO, 
         HE.EMP_NAME, 
         CASE
-            WHEN AVG(HG.SCORE) >= 96
+            WHEN A.AVG_SCORE >= 96
             THEN 'S'
-            WHEN AVG(HG.SCORE) >= 90
+            WHEN A.AVG_SCORE >= 90
             THEN 'A'
-            WHEN AVG(HG.SCORE) >= 80
+            WHEN A.AVG_SCORE >= 80
             THEN 'B'
             ELSE 'C'
         END AS GRADE, 
         CASE
-            WHEN AVG(HG.SCORE) >= 96
+            WHEN A.AVG_SCORE >= 96
             THEN HE.SAL * 0.2
-            WHEN AVG(HG.SCORE) >= 90
+            WHEN A.AVG_SCORE >= 90
             THEN HE.SAL * 0.15
-            WHEN AVG(HG.SCORE) >= 80
+            WHEN A.AVG_SCORE >= 80
             THEN HE.SAL * 0.1
             ELSE 0
         END AS BONUS
-FROM HR_DEPARTMENT HD
-JOIN HR_EMPLOYEES HE
-    ON HE.DEPT_ID = HD.DEPT_ID
-JOIN HR_GRADE HG
-    ON HG.EMP_NO = HE.EMP_NO
-GROUP BY HE.EMP_NO
+FROM HR_EMPLOYEES HE
+JOIN EMP_AVG A
+ON A.EMP_NO = HE.EMP_NO
+ORDER BY HE.EMP_NO

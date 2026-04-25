2877c657adc992ae0d796278a1203d3ac1d58644
Byunghyeon <140622878+bangbang444@users.noreply.github.com>
2026-04-25 22:02:33 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -0,0 +1,18 @@
+-- DEVELOPER 테이블에서 GRADE별 개발자 정보 조회
+SELECT
+        CASE
+            WHEN MAX(S.CATEGORY = 'Front End') = 1 AND MAX(S.NAME = 'Python') = 1
+            THEN 'A'
+            WHEN MAX(S.NAME = 'C#') = 1
+            THEN 'B'
+            WHEN MAX(S.CATEGORY = 'Front End') = 1
+            THEN 'C'
+        END AS GRADE,
+        D.ID, 
+        D.EMAIL
+FROM DEVELOPERS AS D
+    JOIN SKILLCODES AS S
+    ON S.CODE & D.SKILL_CODE = S.CODE
+GROUP BY D.ID
+HAVING GRADE IS NOT NULL
+ORDER BY GRADE, D.ID

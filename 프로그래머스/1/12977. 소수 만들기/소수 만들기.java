de047ca554e9d332ff144ed819ac59af00b9361c
Byunghyeon <140622878+bangbang444@users.noreply.github.com>
2026-06-13 01:21:26 +0900

    [level 1] Title: 소수 만들기, Time: 1.65 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,27 +1,30 @@
 class Solution {
     public int solution(int[] nums) {
-        int length = nums.length;
+        int numsLength = nums.length;
         int count = 0;
-        for(int i = 0; i < length; i++){
-            for(int j = i+1; j < length; j++){
-                for(int k = j+1; k < length; k++){
-                    if(isPrime(nums[i] + nums[j] + nums[k])){
+        for(int i = 0; i < numsLength; i++){
+            for(int j = i+1; j < numsLength; j++){
+                for(int k = j+1; k < numsLength; k++){
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if(isPrime(sum)){
                         count++;
                     }
                 }
             }
         }
 
-        return count;
+        return count++;
     }
     
-    private boolean isPrime(int num){
-        if(num <=1 ) return false;
-        for(int i = 2; i <= Math.sqrt(num); i++){
-            if(num%i==0){
+    public boolean isPrime(int num){
+        if(num == 1) return false;
+        for(int i = 2; i <= (int)Math.sqrt(num); i++){
+            if(num % i == 0){
                 return false;
             }
         }
+        
         return true;
+        
     }
 }
